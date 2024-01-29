0fbff704f26647b917c782a83524362f1332ecec
Goran Radenkovic <goran.cz@outlook.com>
2024-01-29 17:21:24 +0100

    Arrays - JavaScript Basic

---
+++ Homework 12/external.js
@@ -0,0 +1,31 @@
+
+// function tellStory(array) {
+//     if (Array.isArray(array) && array.length === 3 && array.every(item => typeof item === "string")) {
+//       let ime = array[0];
+//       let raspolozenje = array[1];
+//       let aktivnost = array[2];
+//       let prica = `Ovo je ${ime}. ${ime} je dobra osoba. Danas je ${raspolozenje}. Igrao je ${aktivnost} skoro ceo dan.`;
+//       return prica;
+//     } else {
+//       return "Argument mora biti niz od tri stringa.";
+//     }
+//   }
+  
+//   let primer = ["Goran", "respolozen", "sony"];
+//   console.log(tellStory(primer));
+  
+
+// function zbirBrojeva(array) {
+//   if (Array.isArray(array) && array.length === 5 && array.every(n => typeof n === 'number')) {
+//     let sum = 0;
+//     for (let num of array) {
+//       sum += num;
+//     }
+//     return sum;
+//   } 
+// }
+
+// console.log(zbirBrojeva([1, 2, 3, 4, 5])); 
+// console.log(zbirBrojeva([10, 20, 30, 40, 50])); 
+// console.log(zbirBrojeva([1.5, 2.5, 3.5, 4.5, 5.5])); 
+
