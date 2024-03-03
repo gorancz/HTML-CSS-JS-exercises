1158e18e48239e823a2cf7abfd6b660258777690
Goran Radenkovic <goran.cz@outlook.com>
2024-03-03 18:56:53 +0100

    Star Wars Planet Api

---
+++ JS Advanced - Homework 2/script.js
@@ -0,0 +1,38 @@
+const table = document.getElementById('planetsTable');
+const loadPlanets = document.getElementById('loadPlanets');
+const nextPlanets = document.getElementById('next');
+const prevPlanets = document.getElementById('prev');
+
+let page = 1;
+
+const fetchPlanets = (url, nextPage) => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            table.innerHTML = '<tr><th>Planet Name</th><th>Population</th><th>Climate</th><th>Gravity</th></tr>';
+            table.style.border = '1px solid black';
+            data.results.forEach(planet => {
+                table.innerHTML += `<tr><td>${planet.name}</td><td>${planet.population}</td><td>${planet.climate}</td><td>${planet.gravity}</td></tr>`;
+            });
+            nextPlanets.style.display = nextPage ? 'block' : 'none';
+            prevPlanets.style.display = page > 1 ? 'block' : 'none';
+        })
+        .catch(error => console.error('Error fetching planet data:', error));
+};
+
+loadPlanets.addEventListener('click', () => {
+    fetchPlanets('https://swapi.dev/api/planets/?page=1', true);
+});
+
+nextPlanets.addEventListener('click', () => {
+    page++;
+    fetchPlanets('https://swapi.dev/api/planets/?page=' + page, page < 2);
+});
+
+prevPlanets.addEventListener('click', () => {
+    page--;
+    fetchPlanets('https://swapi.dev/api/planets/?page=' + page, true);
+});
+
+
+
